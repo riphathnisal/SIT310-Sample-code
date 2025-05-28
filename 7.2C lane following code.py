40e5c821e630d2d53a13f19c4c808563daae9b50
riphathnisal <164106868+riphathnisal@users.noreply.github.com>
2025-05-28 12:12:55 +1000

    Update 7.2C lane following code.py

--- 7.2C lane following code.py
+++ 7.2C lane following code.py
@@ -1,55 +1,60 @@
 import rospy
-from std_msgs.msg import String
-from duckietown_msgs.msg import Twist2DStamped
-from duckietown_msgs.msg import BoolStamped
-import time
+from duckietown_msgs.msg import Twist2DStamped, BoolStamped
+from std_msgs.msg import Header
+from threading import Timer
 
-class StopSignController:
+class StopSignBehavior:
     def __init__(self):
-        rospy.init_node('stop_sign_controller')
-        
-        self.pub_car_cmd = rospy.Publisher("/birdie/car_cmd", Twist2DStamped, queue_size=1)
-        self.sub_stop_sign = rospy.Subscriber("/birdie/stop_sign_detected", BoolStamped, self.stop_sign_callback)
-        self.sub_lane_cmd = rospy.Subscriber("/birdie/lane_controller/car_cmd", Twist2DStamped, self.lane_cmd_callback)
+        rospy.init_node('stop_sign_behavior_node')
 
-        self.stop_detected = False
-        self.ignore_stop_sign = False
+        self.car_cmd_pub = rospy.Publisher("/birdie/car_cmd", Twist2DStamped, queue_size=1)
+        self.stop_sign_sub = rospy.Subscriber("/birdie/stop_sign_detected", BoolStamped, self.stop_sign_cb)
+        self.lane_follow_sub = rospy.Subscriber("/birdie/lane_controller/car_cmd", Twist2DStamped, self.lane_follow_cb)
+
         self.state = "LANE_FOLLOWING"
-        self.stop_time = None
-        self.ignore_duration = 5  # seconds to ignore Stop Sign after stopping
+        self.ignore_stop_signs = False
+        self.ignore_duration = 4  # seconds
+        self.wait_duration = 3    # stop duration
 
-    def stop_sign_callback(self, msg):
-        if msg.data and not self.ignore_stop_sign and self.state == "LANE_FOLLOWING":
-            rospy.loginfo("Stop sign detected")
+    def stop_sign_cb(self, msg):
+        if self.ignore_stop_signs:
+            return  # Ignore tag callbacks during this window
+        if msg.data and self.state == "LANE_FOLLOWING":
+            rospy.loginfo("Stop Sign Detected!")
             self.state = "STOPPING"
-            self.stop_detected = True
 
-    def lane_cmd_callback(self, msg):
+    def lane_follow_cb(self, msg):
         if self.state == "LANE_FOLLOWING":
-            self.pub_car_cmd.publish(msg)
+            self.car_cmd_pub.publish(msg)
         elif self.state == "STOPPING":
             self.publish_stop()
+            rospy.Timer(rospy.Duration(self.wait_duration), self.resume_after_stop, oneshot=True)
             self.state = "WAITING"
-            self.stop_time = rospy.Time.now().to_sec()
         elif self.state == "WAITING":
-            if rospy.Time.now().to_sec() - self.stop_time > 3:  # 3-second wait
-                rospy.loginfo("Done waiting. Resume lane following and ignore stop sign temporarily.")
-                self.ignore_stop_sign = True
-                self.ignore_start_time = rospy.Time.now().to_sec()
-                self.state = "LANE_FOLLOWING"
-        elif self.ignore_stop_sign:
-            if rospy.Time.now().to_sec() - self.ignore_start_time > self.ignore_duration:
-                self.ignore_stop_sign = False
+            self.publish_stop()
+        elif self.state == "IGNORING_TAGS":
+            self.car_cmd_pub.publish(msg)
 
     def publish_stop(self):
-        stop_cmd = Twist2DStamped()
-        stop_cmd.v = 0
-        stop_cmd.omega = 0
-        self.pub_car_cmd.publish(stop_cmd)
+        stop_msg = Twist2DStamped()
+        stop_msg.header = Header()
+        stop_msg.v = 0.0
+        stop_msg.omega = 0.0
+        self.car_cmd_pub.publish(stop_msg)
+
+    def resume_after_stop(self, event):
+        rospy.loginfo("Stop complete. Resuming lane following and ignoring stop signs for a while.")
+        self.ignore_stop_signs = True
+        self.state = "IGNORING_TAGS"
+        rospy.Timer(rospy.Duration(self.ignore_duration), self.stop_ignoring_tags, oneshot=True)
+
+    def stop_ignoring_tags(self, event):
+        rospy.loginfo("Done ignoring stop signs.")
+        self.ignore_stop_signs = False
+        self.state = "LANE_FOLLOWING"
 
     def run(self):
         rospy.spin()
 
-if __name__ == "__main__":
-    controller = StopSignController()
-    controller.run()
+if __name__ == '__main__':
+    StopSignBehavior().run()
